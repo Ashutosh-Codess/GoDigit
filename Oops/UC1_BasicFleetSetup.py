b61ee60602d3f0aee04d43ad924d97aba971744b
Ashutosh Sharma <ashu19022004@gmail.com>
2026-05-18 22:36:17 +0530

    Oops Practice

--- Oops/UC1_BasicFleetSetup.py
+++ Oops/UC1_BasicFleetSetup.py
@@ -1,1 +1,25 @@
-class
+class Vehicle:
+    def __init__(self, vehicle_id, model, battery_percentage):
+        self.vehicle_id = vehicle_id
+        self.model = model
+        self.battery_percentage = battery_percentage
+
+    def display_details(self):
+        print("\n----- Vehicle Details -----")
+        print("Vehicle ID:", self.vehicle_id)
+        print("Model:", self.model)
+        print("Battery Percentage:", self.battery_percentage, "%")
+
+
+print("Welcome to Eco-Ride Urban Mobility System")
+
+
+vehicle_id = input("Enter Vehicle ID: ")
+model = input("Enter Vehicle Model: ")
+battery_percentage = float(input("Enter Battery Percentage: "))
+
+
+vehicle1 = Vehicle(vehicle_id, model, battery_percentage)
+
+
+vehicle1.display_details()
